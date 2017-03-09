26678647bbf077ec02285a13f86c492a6639be3b
nabergh <nab8kd@virginia.edu>
2017-03-09 12:18:38 -0500

    reward is recorded correctly

--- test_gym.py
+++ test_gym.py
@@ -1,23 +1,27 @@
 import gym
+from itertools import count
 
 env = gym.make('Breakout-ram-v0')
 observation = env.reset()
-print(observation)
-print(env.action_space)
-action = env.action_space.sample()
-observation, reward, done, info = env.step(action)
-print(observation)
-print(reward)
-print(done)
-print(info)
+# print(observation)
+# print(env.action_space)
+# action = env.action_space.sample()
+# observation, reward, done, info = env.step(action)
+# print(observation)
+# print(reward)
+# print(done)
+# print(info)
 
 for i_episode in range(5):
     observation = env.reset()
-    for t in range(100):
+    total_reward = 0
+    for t in count():
         env.render()
-        print(observation)
+        # print(observation)
         action = env.action_space.sample()
         observation, reward, done, info = env.step(action)
+        total_reward += reward
         if done:
             print("Episode finished after {} timesteps".format(t+1))
+            print("Reward " + str(total_reward))
             break
